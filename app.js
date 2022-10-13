944d9334dc9d10f4400876c44ad0671a52022e15
andrewroque <78003117+andrewroque@users.noreply.github.com>
2022-10-13 08:22:26 -0700

    10.13

---
+++ app.js
@@ -0,0 +1,6 @@
+let mouseCursor = document.querySelector(".cursor");
+let navLinks = document.querySelectorAll(.nav-links li');
+
+window.addEventListener('mousemove', cursor);
+
+fuction cursor(e){}
