348cf4d8e33eb91d96c2998a1f66369e4b3d16a9
andrewroque <78003117+andrewroque@users.noreply.github.com>
2022-10-19 19:46:07 -0700

    closer to 1:1 with added halftone

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 alert("HELLO");
 
-new cursoreffects.ghostCursor();
-new cursoreffects.fairyDustCursor({colors: ["#ff0000", "#00ff00", "#0000ff"]});
+// new cursoreffects.ghostCursor();
+// new cursoreffects.fairyDustCursor({colors: ["#ff0000", "#00ff00", "#0000ff"]});
