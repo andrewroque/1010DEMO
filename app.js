f5c3bd48c5b5e6a7a9005f106f7e8736351c0f1e
andrewroque <78003117+andrewroque@users.noreply.github.com>
2022-10-29 21:24:44 -0700

    9pm

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 alert("HELLO");
 
+let button = document.querySelector('.btn')
+
+button.addEventListener('click', ()=>{
+  document.documentElement.classList.toggle('dark-mode')
+})
+
 // new cursoreffects.ghostCursor();
 // new cursoreffects.fairyDustCursor({colors: ["#ff0000", "#00ff00", "#0000ff"]});
