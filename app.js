66840a0b74d704e594c4b103b61c5cadbb69b16a
andrewroque <78003117+andrewroque@users.noreply.github.com>
2022-11-03 02:34:16 -0700

    final 11/3

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 alert("HELLO");
 
-let button = document.querySelector('.btn')
+window.addEventListener("scroll", function(event) {
+  
+  let scroll = this.scrollY;
+  console.log(scroll);
+  if(scroll > 2500) {
+    this.document.body.style.backgroundColor = "#101010";
+    // this.document.body.style.filter = "invert(100%)"; 
+  } else (
+    this.document.body.style.backgroundColor = "#da9d1bf4"
+  )
 
-button.addEventListener('click', ()=>{
-  document.documentElement.classList.toggle('dark-mode')
-})
+  
 
 
+});
 
-function toggleTheme() {
-    // Obtains an array of all <link>
-    // elements.
-    // Select your element using indexing.
-    var theme = document.getElementsByTagName('link')[0];
 
-    // Change the value of href attribute 
-    // to change the css sheet.
-    if (theme.getAttribute('href') == 'light.css') {
-        theme.setAttribute('href', 'dark.css');
-    } else {
-        theme.setAttribute('href', 'light.css');
-    }
-}
